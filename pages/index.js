ef62b560ca32806248b61e25b5156008e54f84a8
Kayla Sween <kayla@kaylasween.com>
2021-04-05 10:23:43 -0500

    adding registration links to home page.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faExternalLinkAlt} from '@fortawesome/free-solid-svg-icons'
 import Layout from '@components/Layout'
 
 export default function Home() {
@@ -12,9 +14,12 @@ export default function Home() {
         <img src="/magnoliajs-2019-26.jpg" alt="Ken Wheeler giving a talk at MagnoliaJS 2019 while attendees watch." />
         <div>
           <p>Join us May 25-26 for MagnoliaJS 2021! This is our last year fully virtual, so we’re going out with a bang!</p>
-          <p>Great speakers, giveaways, and even SWAG BAGS! Register for MagnoliaJS to get a swag bag with stickers and other cool stuff from our sponsors. You may even get a mystery item! <strong>Registration starts on April 8, 2021!</strong></p>
+          <p>Great speakers, giveaways, and even SWAG BAGS! <a href="https://ti.to/magnoliajs/2021" target="_blank" rel="noreferrer">Register for MagnoliaJS</a> today to get a swag bag with stickers and other cool stuff from our sponsors. You may even get a mystery item! <strong>Registration starts on April 8, 2021!</strong></p>
           <p>Check out our <a href="https://twitter.com/MagnoliaJSConf" target="_blank" rel="noreferrer">Twitter</a> to get the latest updates!</p>
         </div>
+      </div>
+      <div className="cta-row">
+        <a href="https://ti.to/magnoliajs/2021" className="cta-btn">Register for MagnoliaJS <FontAwesomeIcon icon={faExternalLinkAlt} /><span className="visually-hidden">External Link</span></a>
       </div>
     </Layout>
   )
