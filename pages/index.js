e2425a03e3b3f96dfb2493098ca8bb87deecaa3f
Kayla Sween <kayla@kaylasween.com>
2021-04-07 21:25:37 -0500

    fixing jankiness on the homepage.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faExternalLinkAlt} from '@fortawesome/free-solid-svg-icons'
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
 import Layout from '@components/Layout'
 
 export default function Home() {
@@ -11,7 +11,9 @@ export default function Home() {
       </Head>
       <h1>MagnoliaJS 2021</h1>
       <div className="about">
-        <img src="/magnoliajs-2019-26.jpg" alt="Ken Wheeler giving a talk at MagnoliaJS 2019 while attendees watch." />
+        <div>
+          <img src="/magnoliajs-2019-26.jpg" alt="Ken Wheeler giving a talk at MagnoliaJS 2019 while attendees watch." />
+        </div>
         <div>
           <p>Join us May 25-26 for MagnoliaJS 2021! This is our last year fully virtual, so we’re going out with a bang!</p>
           <p>Great speakers, giveaways, and even SWAG BAGS! <a href="https://ti.to/magnoliajs/2021" target="_blank" rel="noreferrer">Register for MagnoliaJS</a> today to get a swag bag with stickers and other cool stuff from our sponsors. You may even get a mystery item!</p>
