7acc810a3878729c709fe4e0baaae8355f59dff4
Kayla Sween <kayla@kaylasween.com>
2021-04-16 09:08:48 -0500

    adding virtual coffee as community sponsor.

--- pages/sponsors.js
+++ pages/sponsors.js
@@ -3,6 +3,7 @@ import Layout from '@components/Layout'
 import Sponsor from '@components/Sponsor'
 
 import ThisDotLogo from '../public/Logo-ThisDot.svg'
+import VirtualCoffeeLogo from '../public/virtual-coffee-full.svg'
 import CloudinaryLogo from '../public/cloudinary_logo_blue_0720_svg.svg'
 
 export default function Home() {
@@ -39,6 +40,12 @@ export default function Home() {
       <div className="sponsors">
         <Sponsor link="https://infinite.red" name="Infinite Red">
           <img src="/InfiniteRedLogo.png" alt="Infinite Red's logo." className="sponsor-logo" />
+        </Sponsor>
+      </div>
+      <h2>Community Sponsors</h2>
+      <div className="sponsors">
+        <Sponsor link="https://virtualcoffee.io" name="Virtual Coffee">
+          <VirtualCoffeeLogo />
         </Sponsor>
       </div>
     </Layout>
