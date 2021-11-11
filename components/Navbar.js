6c1a98dfd5cf680fe462f9fd398165531e2f6d9a
Kayla Sween <kayla@kaylasween.com>
2021-11-11 00:13:02 -0500

    changing navigation to blitz routing.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import { Link, Routes } from "blitz"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
