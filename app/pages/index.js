8617aca8f49254bca7724fa2429222f02cba671f
Kayla Sween <kayla@kaylasween.com>
2021-11-14 15:33:54 -0600

    removing husky

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,6 +1,6 @@
-import { Head } from "blitz"
+import { Head } from 'blitz'
 
-import Layout from "../core/layouts/Layout"
+import Layout from '../core/layouts/Layout'
 
 export default function Home() {
   return (
@@ -12,27 +12,14 @@ export default function Home() {
       <h1>See ya in 2022!</h1>
       <div className="about">
         <div>
-          <img
-            src="/magnoliajs-2019-26.jpg"
-            alt="Ken Wheeler giving a talk at MagnoliaJS 2019 while attendees watch."
-          />
+          <img src="/magnoliajs-2019-26.jpg" alt="Ken Wheeler giving a talk at MagnoliaJS 2019 while attendees watch." />
         </div>
         <div>
-          <p>
-            We are getting ready for MagnoliaJS 2022! Follow us on{" "}
-            <a href="https://twitter.com/MagnoliaJSConf" target="_blank" rel="noreferrer">
-              Twitter
-            </a>{" "}
-            to get the latest updates.
-          </p>
+          <p>We are getting ready for MagnoliaJS 2022! Follow us on <a href="https://twitter.com/MagnoliaJSConf" target="_blank" rel="noreferrer">Twitter</a> to get the latest updates.</p>
         </div>
       </div>
       <div className="cta-row">
-        <a
-          href="https://www.youtube.com/channel/UCiP-LfJJjBMANN8JXySXCDA/videos"
-          className="cta-btn"
-        >
-          Check out the 2021 talks on YouTube
+        <a href="https://www.youtube.com/channel/UCiP-LfJJjBMANN8JXySXCDA/videos" className="cta-btn">Check out the 2021 talks on YouTube
           {/* <FontAwesomeIcon icon={faExternalLinkAlt} /><span className="visually-hidden">External Link</span> */}
         </a>
       </div>
