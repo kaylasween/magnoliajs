57f2844799144c9fe8188a040021fa404555b30b
Kayla Sween <kayla@kaylasween.com>
2021-11-14 14:24:16 -0600

    updating home page and code of conduct.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
+
 import Layout from '@components/Layout'
 
 export default function Home() {
@@ -9,13 +10,14 @@ export default function Home() {
       <Head>
         <title>Home | MagnoliaJS</title>
       </Head>
+
       <h1>See ya in 2022!</h1>
       <div className="about">
         <div>
           <img src="/magnoliajs-2019-26.jpg" alt="Ken Wheeler giving a talk at MagnoliaJS 2019 while attendees watch." />
         </div>
         <div>
-          <p>Check out our <a href="https://twitter.com/MagnoliaJSConf" target="_blank" rel="noreferrer">Twitter</a> to get the latest updates!</p>
+          <p>We're getting ready for MagnoliaJS 2022! Follow us on <a href="https://twitter.com/MagnoliaJSConf" target="_blank" rel="noreferrer">Twitter</a> to get the latest updates.</p>
         </div>
       </div>
       <div className="cta-row">
